cb2715e243c028d210742c65004f819589ed16c4
Carlos Guzmán <cguzman@xmltravelgate.com>
2019-03-12 12:02:00 +0100

    Example of use

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/guzmanweb/lab-go-mod
+
+go 1.12
+
+require github.com/guzmanweb/hellomod v0.8.1
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/guzmanweb/hellomod"
+)
 
 func main() {
 	fmt.Println("Hello world! Main function.")
+	hellomod.SayHello()
 }
